aee2970fea1068528b6a6f4f8202ca6da1b5955f
mykhailoleskiv <mykhailo.leskiv@jetsoftpro.com>
2020-08-11 11:24:18 +0300

    Add bottle

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from bottle import route, run
+
+
+@route("/")
+def index():
+    return "Hello, world!"
+
+
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    run(host='localhost', port=8080, debug=True)
